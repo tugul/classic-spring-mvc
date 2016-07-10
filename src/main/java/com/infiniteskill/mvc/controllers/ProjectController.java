848a9caae3e5fa35d53ba5aef8eff84add0390a1
tugul <tugul@live.com>
2016-07-10 14:02:03 +0200

    Used uri template

--- src/main/java/com/infiniteskill/mvc/controllers/ProjectController.java
+++ src/main/java/com/infiniteskill/mvc/controllers/ProjectController.java
@@ -3,6 +3,7 @@ package com.infiniteskill.mvc.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -14,6 +15,13 @@ public class ProjectController {
 	
 	@Autowired
 	private ProjectService projectService;
+	
+	@RequestMapping(value="/{projectId}")
+	public String findProject(Model model, @PathVariable("projectId") Long projectId){
+		// Here path variable projectId is assigned to Long parameter projectId
+		model.addAttribute("project", projectService.find(projectId));
+		return "project";
+	}
 	
 	@RequestMapping(value="/find")
 	public String find(Model model){
